09f02b4758f423e8fb5035b4cd3d88bce4b4fcd8
Z <z-webb@sbcglobal.net>
2026-06-20 15:28:42 -0500

    Prepare app for Render deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //4th add 
 app.use(express.static("public"));
